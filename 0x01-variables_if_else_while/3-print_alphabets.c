d68717937ff8fcf0250f10feebeb830a075c26ef
Lucymuthusi <lucymuthusi@gmail.com>
2023-04-24 16:38:15 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -12,7 +12,7 @@ int main(void)
 {
 	char letter;
 
-	for (letter = 'a'; letter <='z'; letter++)
+	for (letter = 'a'; letter <= 'z'; letter++)
 		putchar(letter);
 
 	for (letter = 'A'; letter <= 'Z'; letter++)
