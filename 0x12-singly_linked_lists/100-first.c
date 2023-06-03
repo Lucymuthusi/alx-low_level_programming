2e0931b9c5e0193ba6e3885f0369181c0cdc171d
Lucymuthusi <lucymuthusi@gmail.com>
2023-06-03 06:01:18 -0700

    0-print_list.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * first - prints a sentence before the main
+ * function is executed
+ */
+void printtext()
+{
+       	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
+int main()
+{
+	printtext();
+	return (0);
+}
