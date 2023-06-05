01fc0b9c00cd372542d95f50cddf588f9b5b6c2d
Lucymuthusi <lucymuthusi@gmail.com>
2023-06-04 22:20:55 -0700

    0-print_list.c

---
+++ 0x12-singly_linked_lists/101-hello_holberton.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * hello_holberton - prints Hello, Holberton
+ * followed by a new line
+ */
+void hello_holberton(void)
+{
+	printf("Hello, Holberton");
+	printf("\n");
+}
+
