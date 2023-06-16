646d17e908ed4c47422a8c25e032734b36489262
Lucymuthusi <lucymuthusi@gmail.com>
2023-06-16 15:33:04 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,41 +1,33 @@
 #include <stdio.h>
 #include "main.h"
 #include <stdlib.h>
-
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 /**
- * read_textfile- reads a text file and prints it to the POSIX standard output.
+ * read_textfile- Read text file print to STDOUT.
  * @filename: text file being read
  * @letters: number of letters to be read
- * Return: the actual number of bytes read and printed
+ * Return: w- actual number of bytes read and printed
  * 0 when function fails or filename is NULL.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	if (filename == NULL)
-	{
-		return (0);
-	}
-	FILE *file = fopen(filename, "r");
+	char *buf;
+	ssize_t fd;
+	ssize_t w;
+	ssize_t t;
 
-	if (file == NULL)
-	{
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
 		return (0);
-	}
-	char buffer[letters + 1];
-	ssize_t bytesRead = fread(buffer, sizeof(char), letters, file);
+	buf = malloc(sizeof(char) * letters);
+	t = read(fd, buf, letters);
+	w = write(STDOUT_FILENO, buf, t);
 
-	if (bytesRead < 0)
-	{
-		fclose(file);
-		return (0);
-	}
-	buffer[bytesRead] = '\0';
-	fclose(file);
-	ssize_t bytesWritten = fwrite(buffer, sizeof(char), bytesRead, stdout);
-
-	if (bytesWritten < 0 || bytesWritten != bytesRead)
-	{
-		return (0);
-	}
-	return (bytesRead);
+	free(buf);
+	close(fd);
+	return (w);
 }
+
--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,27 +13,24 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	if (filename == NULL)
-	{
-		return (-1);
-	}
-	int file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	int fd, w, len = 0;
 
-	if (file == -1)
-	{
+	if (filename == NULL)
 		return (-1);
-	}
+
 	if (text_content != NULL)
 	{
-		ssize_t bytesWritten = write(file, text_content, strlen(text_content));
-
-		if (bytesWritten == -1)
-		{
-			close(file);
-			return (-1);
-		}
+		for (len = 0; text_content[len];)
+			len++;
 	}
-	close(file);
+
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(fd, text_content, len);
+
+	if (fd == -1 || w == -1)
+		return (-1);
+
+	close(fd);
+
 	return (1);
-}
 }
