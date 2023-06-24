9e25d4137fa9b7cbcbb14b2c81ea72f24534426c
Lucymuthusi <lucymuthusi@gmail.com>
2023-06-24 06:24:48 -0700

    updated copy

--- 0x15-file_io/0-main.c
+++ 0x15-file_io/0-main.c
@@ -1,13 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
-#include <stdio.h>
-#include "main.h"
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
 ssize_t read_textfile(const char *filename, size_t letters);
 /**
  * main - check the code
@@ -28,29 +21,4 @@ int main(int ac, char **av)
     n = read_textfile(av[1], 1024);
     printf("\n(printed chars: %li)\n", n);
     return (0);
-}
-
-/**
- * read_textfile- Read text file print to STDOUT.
- * @filename: text file being read
- * @letters: number of letters to be read
- * Return: actual number of bytes read and printed
- * 0 when function fails or filename is NULL.
- */
-ssize_t read_textfile(const char *filename, size_t letters)
-{
-        char *buf = malloc(letters + 1);
-        FILE *fp = fopen(filename, "r");
-        size_t n = fread(buf, 1, letters, fp);
-
-        if (filename == NULL || fp == NULL)
-                return (0);
-        if (n < letters)
-        {
-                buf[n] = '\0';
-        }
-        write(STDOUT_FILENO, buf, n);
-        fclose(fp);
-        free(buf);
-        return (n);
 }
