94441211477c6ac438c031978a7eef4078cf0146
Lucymuthusi <lucymuthusi@gmail.com>
2025-03-15 03:41:25 -0700

    update

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,7 +11,7 @@ void times_table(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j < 10;j++)
+		for (j = 0; j < 10; j++)
 		{
 			k = j * i;
 			if (j == 0)
@@ -19,7 +19,8 @@ void times_table(void)
 				_putchar(k + '0');
 			}
 
-			if (k < 10 && j ! = 0)
+			if (k < 10)
+				&&(j != 0))
 			{
 				_putchar(',');
 				_putchar(',');
