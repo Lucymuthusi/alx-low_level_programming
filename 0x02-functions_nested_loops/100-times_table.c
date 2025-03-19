61b3964b4c56c973da55db9ff7206bb18b61e451
Lucymuthusi <lucymuthusi@gmail.com>
2025-03-19 03:25:12 -0700

    update

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -17,24 +17,22 @@ void print_times_table(int n)
 			for (j = 0; j <= n; j++)
 			{
 				k = j * i;
+				if (j != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+				}
+				if (k < 10)
 				{
 					_putchar(k + '0');
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar('\n');
-				}
-				if (k >= 10 && k < 100)
+				} else
 				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
 					_putchar((k / 10) + '0');
 					_putchar((k % 10) + '0');
-					_putchar('\n');
 				}
+			}
 		}
-	}
 	_putchar('\n');
-}
+	}
 }
