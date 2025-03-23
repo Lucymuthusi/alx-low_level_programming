8a7e2e7f2216c68c6b3140c731fa376cc741a82f
Lucymuthusi <lucymuthusi@gmail.com>
2025-03-22 23:44:43 -0700

    update

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,11 +12,11 @@ int i;
 
 for (i = 0; i < (n - 1); i++)
 {
-	printf("%d,", a[i]);
+	printf("%d,",  a[i]);
 }
 if (i == (n - 1))
 {
-	printf("%d", a[n - 1]);
+	printf("%d",  a[n - 1]);
 }
 printf("\n");
 }
