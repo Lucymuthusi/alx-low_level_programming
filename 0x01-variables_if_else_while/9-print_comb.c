4c5f07067e885d669545265a6026ea93b51d09b2
Lucymuthusi <lucymuthusi@gmail.com>
2025-04-03 08:35:33 -0700

    update

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,20 +8,17 @@
  */
 int main(void)
 {
-	int i, j;
+	int i;
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j < 10; j++)
+		putchar(i + '0');
+		if (i < 9)
 		{
-			if (i <= j)
-			{
-				putchar(i / 10 + '0');
-				putchar(j + '0');
-				putchar(',');
-				putchar(' ');
-			}
+		putchar(',');
+		putchar(' ');
 		}
 	}
+	putchar('\n');
 	return (0);
 }
